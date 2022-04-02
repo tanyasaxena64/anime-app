25d50d687e30a5af0f3a3324f5df3f96f2bfeea3
Tanya <anshisaxena64@gmail.com>
2022-04-02 22:58:21 +0530

    final code

---
+++ src/components/AnimeCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function AnimeCard(anime) {
+    console.log('11', anime)
+  return (
+    <article className="anime-card">
+        <a href={anime.url} target="_blank" rel="noreferrer">
+            <figure>
+                <img src={anime.anime.image_url} alt="Anime Image"/>
+            </figure>
+            <h3>{anime.title}</h3>
+        </a>
+    </article>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <header><h1>My<strong>Anime</strong>Data</h1></header>
+  )
+}
