d9701d07ee4cdafa04fd596789a8b0fe05de4c04
Tanya <anshisaxena64@gmail.com>
2022-04-02 23:19:40 +0530

    commit message

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
 export default function Header() {
   return (
-    <header><h1>My<strong>Anime</strong>Data</h1></header>
+    <header>
+        <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+        <h1>My<strong>Anime</strong>Data</h1>
+        <h4>(Made By -<strong style={{color:'green'}}> Tanya)</strong></h4>
+
+        </div>
+    </header>
   )
 }
